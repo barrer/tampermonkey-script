7bdca710a4137ff872036e6236be0c9ccaf4e99e
example <example@example.com>
2017-08-03 07:29:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Tampermonkey油猴子脚本
+
+Tampermonkey 是一款免费的浏览器扩展和最为流行的用户脚本管理器，它适用于 Chrome, Microsoft Edge, Safari, Opera Next, 和 Firefox。 
+
+虽然有些受支持的浏览器拥有原生的用户脚本支持，但 Tampermonkey 将在您的用户脚本管理方面提供更多的便利。 它提供了诸如便捷脚本安装、自动更新检查、标签中的脚本运行状况速览、内置的编辑器等众多功能， 同时Tampermonkey还有可能正常运行原本并不兼容的脚本。 
+
+它可以很快的安装好，来试一试吧！
---
+++ v2ex-preview/v2ex-preview.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         v2ex-preview
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  https://github.com/barrer/tampermonkey-script/v2ex-preview/
+// @author       barrer
+// @match        https://www.v2ex.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var interval = 3;// 秒，每隔几秒ajax GET下一个帖子（太频繁会被v2ex.com封IP，建议：大于等于3秒）
+    function gAjaxGet(url, fnSuccess, fnError, element) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) {
+                    fnSuccess(xmlhttp.responseText, element);
+                } else {
+                    fnError(xmlhttp.responseText, element);
+                }
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+
+    function appendHtml(response, element) {
+        // console.log(response, element);
+        // 获取返回内容正文
+        var result = '';
+        var parser = new DOMParser();
+        var resultDoc = parser.parseFromString(response, 'text/html');
+        var resultContent = resultDoc.querySelector('#Main .box .cell .topic_content');
+        if(resultContent) {
+            result = resultContent.innerText;
+        }
+        var resultSubContent = resultDoc.querySelector('#Main .box .subtle .topic_content');
+        if(resultSubContent) {
+            result += resultSubContent.innerText;
+        }
+        result = result.replace(/\n/g, '<br>');
+        // 把正文插入标题后面
+        var content = document.createElement('div');
+        content.innerHTML = '<div>' + result + '</div>';
+        element.parentElement.append(content);
+    }
+
+    var list = document.querySelectorAll('.item_title');
+    list.forEach(function(item, index) {
+        var a = item.querySelector('a');
+        setTimeout(function() {
+            gAjaxGet(a.getAttribute('href'), function(response, element) {
+                // 获取内容成功
+                appendHtml(response, element);
+            }, function(response, element) {
+                // 获取内容出错（添加自定义错误提示）
+                appendHtml('<div id="Main"><div class="box"><div class="cell"><div class="topic_content">暂未能获取内容！（' +
+                           response +
+                           '）</div></div></div></div>', element);
+            }, item);
+        }, index * interval * 1000 + 2000);
+    });
+})();
