dad1f060d3d95f7c8f6afe9e282456f143320c78
ivysrono <ivysrono@users.noreply.github.com>
2019-03-31 13:29:38 +0800

    Update and rename translate.js to translate.user.js

--- translate/translate.user.js
+++ translate/translate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Translate
 // @namespace    http://tampermonkey.net/
-// @version      5.5
+// @version      5.5.1
 // @description  划词翻译调用“金山词霸、有道词典（有道翻译）、Google Translate（谷歌翻译）、沪江小D、搜狗翻译、必应词典（必应翻译）、Microsoft Translator（必应在线翻译）、海词词典、百度翻译、Oxford Learner's Dictionaries、Oxford Dictionaries、Merriam-Webster、汉典、PDF 划词翻译、Google Search、Bing Search（必应搜索）、百度搜索、Wikipedia Search（维基百科搜索）”网页翻译
 // @author       https://github.com/barrer
 // @match        http://*/*
