be5b56b61239e3e7afb236f4e5806bee69d6b0f7
example <example@example.com>
2020-11-22 16:28:59 +0800

    修复谷歌翻译

--- translate/translate.js
+++ translate/translate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Translate
 // @namespace    http://tampermonkey.net/
-// @version      8
+// @version      9
 // @description  划词翻译调用“金山词霸、有道词典（有道翻译）、Google Translate（谷歌翻译）、沪江小D、搜狗翻译、必应词典（必应翻译）、Microsoft Translator（必应在线翻译）、DeepL翻译、海词词典、百度翻译、Oxford Learner's Dictionaries、Oxford Dictionaries、Merriam-Webster、PDF 划词翻译、Google Search、Bing Search（必应搜索）、百度搜索、Wikipedia Search（维基百科搜索）”网页翻译
 // @author       https://github.com/barrer
 // @match        http://*/*
@@ -114,7 +114,7 @@
                     popupCenter('https://translate.google.com', null, 800, screen.height);
                 },
                 custom: function (text) {
-                    var source = document.querySelector('#source');
+                    var source = document.querySelectorAll('textarea')[0];
                     source.value = text;
                     triggerEvent(source, 'input');
                     triggerEvent(source, 'keyup');
@@ -183,7 +183,7 @@
                     popupCenter('https://translate.google.cn', null, 800, screen.height);
                 },
                 custom: function (text) {
-                    var source = document.querySelector('#source');
+                    var source = document.querySelectorAll('textarea')[0];
                     source.value = text;
                     triggerEvent(source, 'input');
                     triggerEvent(source, 'keyup');
