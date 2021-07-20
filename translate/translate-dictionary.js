55323cc053974dc8221406276f2d069b9fc559d2
example <example@example.com>
2021-07-20 12:12:09 +0800

    添加开源许可证

--- translate/translate-dictionary.js
+++ translate/translate-dictionary.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         划词翻译：多词典查询
 // @namespace    http://tampermonkey.net/
-// @version      10.6
+-// @version      10.7
 // @description  划词翻译调用“有道词典（有道翻译）、金山词霸、Bing 词典（必应词典）、剑桥高阶、沪江小D、谷歌翻译”
 // @author       https://github.com/barrer
+// @license      http://www.apache.org/licenses/LICENSE-2.0
 // @match        http://*/*
 // @include      https://*/*
 // @include      file:///*
@@ -16,6 +17,27 @@
 // @connect      cambridge.org
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
+
+/*
+Copyright https://github.com/barrer
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.    
+*/
 
 (() => {
     'use strict';
--- translate/translate.js
+++ translate/translate.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Translate
 // @namespace    http://tampermonkey.net/
-// @version      10.5
+// @version      10.6
 // @description  划词翻译调用“金山词霸、有道词典（有道翻译）、Google Translate（谷歌翻译）、沪江小D、搜狗翻译、必应词典（必应翻译）、Microsoft Translator（必应在线翻译）、DeepL翻译、海词词典、百度翻译、Oxford Learner's Dictionaries、Oxford Dictionaries、Merriam-Webster、PDF 划词翻译、Google Search、Bing Search（必应搜索）、百度搜索、Wikipedia Search（维基百科搜索）”网页翻译
 // @author       https://github.com/barrer
+// @license      http://www.apache.org/licenses/LICENSE-2.0
 // @match        http://*/*
 // @include      https://*/*
 // @include      file:///*
@@ -11,6 +12,27 @@
 // @grant        GM_getValue
 // @grant        GM_deleteValue
 // ==/UserScript==
+
+/*
+Copyright https://github.com/barrer
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.    
+*/
 
 (() => {
     'use strict';
