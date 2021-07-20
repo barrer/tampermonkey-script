247d50bbf9fe0c51c8ddb8837b3b0ecdc16091b7
example <example@example.com>
2021-07-20 12:15:18 +0800

    修正拼写错误

--- translate/translate-dictionary.js
+++ translate/translate-dictionary.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         划词翻译：多词典查询
 // @namespace    http://tampermonkey.net/
--// @version      10.7
+// @version      10.8
 // @description  划词翻译调用“有道词典（有道翻译）、金山词霸、Bing 词典（必应词典）、剑桥高阶、沪江小D、谷歌翻译”
 // @author       https://github.com/barrer
 // @license      http://www.apache.org/licenses/LICENSE-2.0
